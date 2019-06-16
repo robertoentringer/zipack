37be964f4dd42e873f6420c05610f7a9ca327736
Roberto Entringer <r.entringer@gmail.com>
2019-06-16 03:54:53 +0200

    Initial commit

---
+++ src/zipack.js
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+
+const fs = require("fs")
+const path = require("path")
+const archiver = require("archiver")
+
+const error = err => {
+  console.error(`\n${err.message}`)
+  console.error(`Please check the manifest file \x1b[31m${file}\x1b[0m and try again\n`)
+  process.exit()
+}
+
+const file = process.argv.slice(2)[0] || error(new Error("Manifest file is missing or unreadable"))
+
+const filename = (() => {
+  try {
+    const { name, version } = JSON.parse(fs.readFileSync(file, "utf-8"))
+    if (name && version) return `${name} v${version}.zip`
+    error(new Error(`Required value 'name or version' is missing or invalid.`))
+  } catch (err) {
+    error(err)
+  }
+})()
+
+const stream = fs.createWriteStream(filename)
+stream.on("open", () => console.info(`\nZipping... ${filename}`))
+stream.on("close", () => console.info("Zip ok!", "\n"))
+stream.on("error", error)
+
+const archive = archiver("zip", { zlib: { level: 9 } })
+archive.directory(path.dirname(file), false)
+archive.on("error", error)
+archive.pipe(stream)
+archive.finalize()
