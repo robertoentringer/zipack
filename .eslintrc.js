8a4e1cc9cf8fa8b0e2ebf7e61399f31622898d72
Roberto Enringer <r.entringer@gmail.com>
2020-07-13 15:10:18 +0200

    fix: bump npm packages to latest and config semantic-release

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,16 @@ module.exports = {
     node: true,
     commonjs: true,
     browser: true,
-    es6: true,
+    es6: true
   },
   extends: ["eslint:recommended", "plugin:prettier/recommended"],
-  globals: {
-    chrome: "readonly",
-  },
   plugins: ["prettier"],
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module",
+    sourceType: "module"
   },
   rules: {
-    "prettier/prettier": ["error", { semi: false, printWidth: 100}],
     "comma-dangle": ["error", "never"],
-    "no-console": ["error", { allow: ["error", "info"] }],
-  },
+    "no-console": ["error", { allow: ["error"] }]
+  }
 }
